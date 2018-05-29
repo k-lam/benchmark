4015065f8a23e06b76600af73892be05bebd9be1
kl in ubuntu <k.lamgreedy@gmail.com>
2018-05-29 15:03:29 +0800

    modify exec_all.sh

--- exec_all.sh
+++ exec_all.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export EXAMPLES_HOME=/home/mmm-dl-ml-l/kl/code/examples
+export EXAMPLES_HOME=~/kl/code/examples
 scripts/model_accuracy.py --filename klbenchmark.log --data-dir /home/mmm-dl-ml-l/data/tiny-imagenet-200
